2ab89df7ecbf39a2fd8635b2b72f01ae5cf755a8
Kathleen_M <kathleen.malialin@gmail.com>
2019-12-10 09:56:10 -0300

    add-array-pdts

--- lister-produits.php
+++ lister-produits.php
@@ -13,11 +13,16 @@
         <li>Croco</li>
     </ul>
    <?php
-    $produits = [
-                'Tagada' => ['prix' => 1, 'quantite' => 10],
-                'Schtroumpfs' => ['prix' => 1.5, 'quantite' => 10],
-                'Croco' => ['prix' => 2, 'quantite' => 10]
-            ]
+    // $produits = [
+    //             'Tagada' => ['prix' => 1, 'quantite' => 10],
+    //             'Schtroumpfs' => ['prix' => 1.5, 'quantite' => 10],
+    //             'Croco' => ['prix' => 2, 'quantite' => 10]
+    //         ]
+            $produit=array();
+            $produit['nom']="Tagada";
+            $produit['prix']=1;
+            $produit['quantite']=50;
+            echo " Article: " .$produit['nom'] ." Prix (€): " .$produit['prix'] ." Quantité: " .$produit['quantite'];
         ?>
 </body>
 </html>
