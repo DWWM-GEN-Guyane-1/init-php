84399c9d230705534a9d630bec45d802f3216125
Kathleen_M <kathleen.malialin@gmail.com>
2019-12-10 10:03:12 -0300

    add-h1 && array-pdts+echo

--- lister-produits.php
+++ lister-produits.php
@@ -7,6 +7,7 @@
     <title>Magasin</title>
 </head>
 <body>
+<h1>Articles en stocks - marque Haribo</h1>
     <ul>
         <li>Tagada</li>
         <li>Schtroumpfs</li>
@@ -21,6 +22,18 @@
             $produit=array();
             $produit['nom']="Tagada";
             $produit['prix']=1;
+            $produit['quantite']=50;
+            echo " Article: " .$produit['nom'] ." Prix (€): " .$produit['prix'] ." Quantité: " .$produit['quantite'] ."||";
+        
+            $produit=array();
+            $produit['nom']="Croco";
+            $produit['prix']=1.5;
+            $produit['quantite']=50;
+            echo " Article: " .$produit['nom'] ." Prix (€): " .$produit['prix'] ." Quantité: " .$produit['quantite'] ."||";
+        
+            $produit=array();
+            $produit['nom']="Schtroumpfs";
+            $produit['prix']=2;
             $produit['quantite']=50;
             echo " Article: " .$produit['nom'] ." Prix (€): " .$produit['prix'] ." Quantité: " .$produit['quantite'];
         ?>
